7b6ece07951756385318646355d7c92157e6e270
Caldwell <caldwell@maxoof.net>
2018-10-17 19:31:24 +0800

    调整动画

--- nest-vue/src/App.vue
+++ nest-vue/src/App.vue
@@ -42,65 +42,36 @@
   .slide-down-enter-active,
   .slide-down-leave-active {
     position: absolute;
-    will-change: transform;
-    transition: all 500ms;
+    transform: translate3d(0, 0, 0);
+    transition: transform .5s;
   }
 
   .slide-left-enter {
-    position: absolute;
     transform: translate3d(100%, 0, 0);
-    z-index: 2;
-  }
-
-  .slide-left-enter-active {
-    z-index: 2;
-  }
-
-  .slide-left-leave-active {
-    transform: translate3d(0, 0, 0);
-    z-index: 1;
-  }
-
-  .slide-right-enter {
-    position: absolute;
-    z-index: 1;
-  }
-
-  .slide-right-enter-active {
     z-index: 1;
   }
 
   .slide-right-leave-active {
     transform: translate3d(100%, 0, 0);
-    z-index: 2;
+    z-index: 1;
+  }
+
+  .slide-right-leave {
+    transform: translate3d(0, 0, 0);
   }
 
   .slide-top-enter {
-    position: absolute;
     transform: translate3d(0, 100%, 0);
-    z-index: 2;
-  }
-
-  .slide-top-enter-active {
-    z-index: 2;
-  }
-
-  .slide-top-leave-active {
-    transform: translate3d(0, 0, 0);
-    z-index: 1;
-  }
-
-  .slide-down-enter {
-    position: absolute;
-    z-index: 1;
-  }
-
-  .slide-down-enter-active {
     z-index: 1;
   }
 
   .slide-down-leave-active {
     transform: translate3d(0, 100%, 0);
-    z-index: 2;
+    z-index: 1;
   }
+
+  .slide-down-leave {
+    transform: translate3d(0, 0, 0);
+  }
+
 </style>
