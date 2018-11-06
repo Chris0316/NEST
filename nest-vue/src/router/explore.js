adf555a089551de8c620fbd6c3dde46ced936f5b
Caldwell <caldwell@maxoof.net>
2018-11-06 22:39:10 +0800

    路由调整

--- nest-vue/src/router/explore.js
+++ nest-vue/src/router/explore.js
@@ -2,12 +2,12 @@ import Explore from '@/views/Explore'
 import List from '@/views/List'
 import Search from '@/views/Search'
 import Details from '@/views/Details'
-import Pulish from '@/views/Pulish'
+import Publish from '@/views/Publish'
 
 export default [
   { path: '/explore', name: 'Explore', component: Explore },
   { path: '/list/:type', name: 'List', component: List },
   { path: '/search', name: 'Search', component: Search },
   { path: '/details/:type/:id', name: 'Details', component: Details },
-  { path: '/pulish', name: 'Pulish', component: Pulish }
+  { path: '/explore/publish', name: 'Publish', component: Publish }
 ]
