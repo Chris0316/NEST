54883fae24bf3e82b1df8a6cee2bb5d644eb061a
sunguide <sunguide@qq.com>
2018-11-18 11:29:06 +0800

    增加响应处理

--- nest-vue/src/utils/Request.js
+++ nest-vue/src/utils/Request.js
@@ -16,7 +16,19 @@ instance.interceptors.request.use(config => {
 
 instance.interceptors.response.use(response => {
   console.log('响应接收前');
-  return response;
+  /**
+   * 下面的注释为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
+   * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
+   */
+  const res = response.data;
+  if (res.status == 401) {
+    //未授权跳转到登录
+  }else if(res.status >= 300 || res.status < 200){
+    //请求失败
+    return Promise.reject('error');
+  } else {
+    return response.data;
+  }
 }, error => {
   console.log('响应发生错误');
   return Promise.reject(error);
