bb35202c89290caf93f620adc7a7de9006367cb0
Caldwell <caldwell@maxoof.net>
2018-11-24 13:48:24 +0800

    scroller调整

--- nest-vue/src/views/my/Ground.vue
+++ nest-vue/src/views/my/Ground.vue
@@ -10,7 +10,13 @@
         <button class="control-btn" @click="locationShow = true">区域</button>
       </div>
     </div>
-    <nest-scroll class="scroll-wrapper">
+    <nest-scroll class="scroll-wrapper"
+                 ref="scroll"
+                 :pullDownRefresh="pullDownRefreshObj"
+                 :pullUpLoad="pullUpLoadObj"
+                 :startY="parseInt(startY)"
+                 @pullingDown="onPullingDown"
+                 @pullingUp="onPullingUp">
       <div class="list">
         <nest-swipe-cell v-for="(item, index) in options" :key="index" class="list-item">
           <div class="item"
@@ -58,7 +64,66 @@
           { title: '公寓', type: 'buy', budget: '400-500万Peso', area: '马卡提（Makati）', date: '2018-08-19' },
           { title: '公寓', type: 'rent', budget: '400-500万Peso', area: '马卡提（Makati）', date: '2018-08-19' },
           { title: '公寓', type: 'rent', budget: '400-500万Peso', area: '马卡提（Makati）', date: '2018-08-19' }
-        ]
+        ],
+        // 这个配置用于做下拉刷新功能，默认为 false。当设置为 true 或者是一个 Object 的时候，可以开启下拉刷新，可以配置顶部下拉的距离（threshold） 来决定刷新时机以及回弹停留的距离（stop）
+        pullDownRefreshObj: {
+          threshold: 90,
+          stop: 40
+        },
+        // 这个配置用于做上拉加载功能，默认为 false。当设置为 true 或者是一个 Object 的时候，可以开启上拉加载，可以配置离底部距离阈值（threshold）来决定开始加载的时机
+        pullUpLoadObj: {
+          threshold: 0,
+          txt: {
+            more: '加载更多',
+            noMore: '没有更多数据了'
+          }
+        },
+        startY: 0,  // 纵轴方向初始化位置
+        scrollToX: 0,
+        scrollToY: 0,
+        scrollToTime: 700,
+      }
+    },
+    mounted () {
+      this.onPullingDown()
+    },
+    methods: {
+      // 滚动到页面顶部
+      scrollTo () {
+        this.$refs.scroll.scrollTo(this.scrollToX, this.scrollToY, this.scrollToTime)
+      },
+      // 模拟数据请求
+      getData () {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            const arr = []
+            for (let i = 0; i < 20; i++) {
+              arr.push({ title: '公寓', type: 'rent', budget: '400-500万Peso', area: '马卡提（Makati）', date: '2018-08-19' })
+            }
+            resolve(arr)
+          }, 1000)
+        })
+      },
+      onPullingDown () {
+        // 模拟下拉刷新
+        console.log('下拉刷新')
+        // count = 0
+        this.getData().then(res => {
+          this.options = res
+          this.$refs.scroll.forceUpdate(true)
+        })
+      },
+      onPullingUp () {
+        // 模拟上拉 加载更多数据
+        console.log('上拉加载')
+        this.getData().then(res => {
+          this.options = this.options.concat(res)
+          if(this.options.length<50){
+            this.$refs.scroll.forceUpdate(true)
+          }else{
+            this.$refs.scroll.forceUpdate(false)
+          }
+        })
       }
     }
   }
@@ -127,9 +192,6 @@
     .scroll-wrapper {
       height: 300px;
       overflow: hidden;
-    }
-    .list {
-      margin-top: .6rem;
     }
     .list-item {
       margin-bottom: .4rem;
